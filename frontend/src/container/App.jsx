5d97a50c9af5d4131f229d4e60547ead2e8c65fc
Arpit Bhalla <arpitbhalla2001@gmail.com>
2020-11-25 10:23:16 +0530

    Improved

--- frontend/src/container/App.jsx
+++ frontend/src/container/App.jsx
@@ -1,29 +1,21 @@
 import React from "react";
 import App from "screens";
 import { ThemeProvider } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
 import { Provider } from "react-redux";
 import { SnackbarProvider } from "notistack";
 import theme from "core/theme";
 import store from "common/store";
-import firebase from "core/firebase";
-
-const rrfProps = {
-  firebase,
-  config: rrfConfig,
-  dispatch: store.dispatch,
-  createFirestoreInstance,
-};
 
 export default () => (
   <React.StrictMode>
     <Provider store={store}>
-      <ReactReduxFirebaseProvider {...rrfProps}>
-        <ThemeProvider theme={theme}>
-          <SnackbarProvider>
-            <App />
-          </SnackbarProvider>
-        </ThemeProvider>
-      </ReactReduxFirebaseProvider>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <SnackbarProvider>
+          <App />
+        </SnackbarProvider>
+      </ThemeProvider>
     </Provider>
   </React.StrictMode>
 );
